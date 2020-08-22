f3910b5777cfa7e08fffd9bd28855851722c1a43
trod <st104680@stud.uni-stuttgart.de>
2020-08-22 12:25:26 +0200

    Sending messages to MQ SLADIM-151

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { LogMessageFormat } from "logging-format";
+const {Kafka} = require('kafkajs');
+
+const kafka = new Kafka({
+  clientId: 'prometheus-alert-converter',
+  brokers: ['localhost:9092']
+});
+const producer = kafka.producer();
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  /**
+   * Sends the log message to the kafka topic logs
+   * 
+   * @param logMessage log in the LogMessageFormat to be send to the issue creator
+   */
+  async sendLogMessage(logMessage: LogMessageFormat) {
+    await producer.connect();
+    await producer.send({
+      topic: 'logs',
+      messages: [{
+value: JSON.stringify(logMessage)}]
+    });
+    await producer.disconnect();
   }
 }
