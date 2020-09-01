802ed21e4ec18c0c736f5f504f768a7a1c3db03c
Marcel Wolkober <st163937@stud.uni-stuttgart.de>
2020-09-01 15:37:49 +0200

    Fix 3

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,7 +10,8 @@ export class AppController {
   constructor(private readonly appService: AppService) { }
 
   /**
-   * Example Request to Test whether the Sample-Alerts.json File can be converted into LogMessages.
+   * Example Request to Test whether the Sample-Alerts.json File can be converted into LogMessages.   * 
+   * 
    * @returns a resolved Promise containing the converted LogMessages.
    */
   @Get('/get-sample')
@@ -21,9 +22,12 @@ export class AppController {
   /**
    * Endpoint for the Alert-Manager, incoming Alerts will be converted and sent to the Kafka Queue.
    * For more details of the Queue see: https://ccims.github.io/overview-and-documentation/error-response-monitor.
+   * 
+   * @param alerts the Alerts to convert.
+   * @returns A resolved Promise.
    */
   @Post('post-alerts')
-  addAlertsToConvert(@Body() alerts) {
+  addAlertsToConvert(@Body() alerts): Promise<any>  {
     return this.appService.sendConvertedLogs(alerts);
   }
 
